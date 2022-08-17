cee8098c22ed4da609479c4a53faf431d1b5cbb1
Madad <madad.ali7897@gmail.com>
2022-08-17 06:08:41 -0700

    Auth-Guard Work

--- WEB/src/app/app-routing.module.ts
+++ WEB/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { ConfirmEmailComponent } from './confirm-email/confirm-email.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LogoutPageComponent } from './logout-page/logout-page.component';
@@ -8,13 +9,14 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 const routes: Routes = [
-  {path:'',component:StudentDashboardComponent},
-  {path:'dashboard',component:StudentDashboardComponent},
+  {path:'', redirectTo: 'login',pathMatch: 'full' },
+  {path:'dashboard', canActivate: [AuthGuard] ,component:StudentDashboardComponent},
   {path:'logout',component:LogoutPageComponent},
   {path:'login',component:LoginPageComponent},
   {path:'register',component:RegisterPageComponent},
   {path:'password/reset',component:ConfirmEmailComponent},
-  {path:'account/password/reset/:id',component:PasswordResetComponent}
+  {path:'account/password/reset/:id',component:PasswordResetComponent},
+  {path: '**', redirectTo: 'login'}
 ];
 
 @NgModule({
---
+++ WEB/src/app/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Token } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../shared/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const token = localStorage.getItem('token');
+      if(token){
+        return true;
+      }
+      return this.router.createUrlTree(['']);
+
+  }
+  
+}
